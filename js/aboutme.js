8748bdd076f85f97cc932f394b046765a21f04a9
NGUYEMINHKHA <127472800+NguyenMinhKhaBT@users.noreply.github.com>
2023-03-11 17:24:57 +0700

    2
    524

--- js/aboutme.js
+++ js/aboutme.js
@@ -5,7 +5,7 @@ var div = document.getElementById('content');
 
 
 button1.onclick = function() {
-    div.innerHTML = '<li>Địa chỉ: 99/5 – Nguyễn Văn Cừ, <br>P.An Khánh, Q.Ninh Kiều, TP.CT</li><li>Điện thoại: +84292 3832 663</li> <li>Fax: +84292 3838474</li> <li>Email: nva @ctu.edu.vn</li> ';
+    div.innerHTML = '<li>Địa chỉ: 249 – Tich Phuc, Khanh Thanh Tan, Mo Cay Bac, Ben Tre</li><li>Điện thoại: +84 3 65209761</li> <li>Work: +84 8 65879617</li> <li>Email: ngminhkha.bt@gmail.com</li> ';
 };
 
 button2.onclick = function() {
